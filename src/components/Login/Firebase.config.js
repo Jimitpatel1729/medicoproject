ecee7ec75cc1e31949e6f3ba7e52c287fc4ff926
Jimitpatel1729 <jimitmpatel68@gmail.com>
2021-03-19 11:04:19 +0530

    first commit

--- src/components/Login/Firebase.config.js
+++ src/components/Login/Firebase.config.js
@@ -1,10 +1,19 @@
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCaw91TOJaIJzLH5c7yFOMvmtpQwjBcyUk",
+//   authDomain: "doctors-portal-52744.firebaseapp.com",
+//   databaseURL: "https://doctors-portal-52744.firebaseio.com",
+//   projectId: "doctors-portal-52744",
+//   storageBucket: "doctors-portal-52744.appspot.com",
+//   messagingSenderId: "335976826950",
+//   appId: "1:335976826950:web:492c7825283a55ae3c6247"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCaw91TOJaIJzLH5c7yFOMvmtpQwjBcyUk",
-  authDomain: "doctors-portal-52744.firebaseapp.com",
-  databaseURL: "https://doctors-portal-52744.firebaseio.com",
-  projectId: "doctors-portal-52744",
-  storageBucket: "doctors-portal-52744.appspot.com",
-  messagingSenderId: "335976826950",
-  appId: "1:335976826950:web:492c7825283a55ae3c6247"
-};
+  apiKey: "AIzaSyAvmGOVr9nSlW6PWi-krSbQPIMRhroLuWM",
+  authDomain: "medico-54f0b.firebaseapp.com",
+  projectId: "medico-54f0b",
+  storageBucket: "medico-54f0b.appspot.com",
+  messagingSenderId: "1013126274817",
+  appId: "1:1013126274817:web:f75d397d40c562a8144500",
+  measurementId: "G-WCSJ62LGR0"};
+
 export default firebaseConfig;
